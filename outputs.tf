d9aaa7a095f4b70dce4828c3de44e11f4732e8b3
etwillbefine <tim.gatzemeier@gmx.de>
2020-03-22 00:28:09 +0100

    correct reference to single commit resource

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "content_hash" {
 }
 
 output "done_trigger" {
-  value       = join("", null_resource.commits.*.triggers.hash)
+  value       = join("", null_resource.commit.*.triggers.hash)
   description = "As this references the cleanup resources you can rely on it to determine the commits are made."
 }
